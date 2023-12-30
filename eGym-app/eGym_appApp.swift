f33540cbdbbe28bb25e3ecb20dc0087847e531da
eduardferre <eduardferresanchez@gmail.com>
2023-12-30 15:59:21 +0100

    first commit

---
+++ eGym-app/eGym_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  eGym_appApp.swift
+//  eGym-app
+//
+//  Created by Eduard Ferré Sánchez on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct eGym_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
